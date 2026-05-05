b1c71c61c5b53ac85c4d748d3672e99e6a6c1aaf
Suhas Shinde <engr.suhas.shinde@gmail.com>
2026-05-05 12:58:48 +0530

    Feature: Add chat session with JSON persistence

--- agent.py
+++ agent.py
@@ -1,11 +1,36 @@
 from openai import OpenAI
+import os
+import json
+from datetime import datetime
+
+SYSTEM_PROMPT = (
+    "You are an expert coding assistant. "
+    "Be concise and clear in your responses."
+)
+CHAT_DIR = "chat_history"
+
+def create_chat_file():
+    os.makedirs(CHAT_DIR, exist_ok = True)
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    return f"{CHAT_DIR}/chat_{timestamp}.json"
+
+def save_conversation(filename, conversation):
+    with open(filename, "w") as f:
+        json.dump(conversation, f, indent = 4)
 
 client = OpenAI(
     base_url = "http://localhost:11434/v1",
     api_key = "ollama"
 )
 
-conversation = []
+chat_file = create_chat_file()
+
+conversation = [
+    {
+        "role": "system",
+        "content": SYSTEM_PROMPT
+    }
+]
 
 print("Suhas's Coding Assistant (🤖): Hello! (type 'exit' to quit)")
 print("=" * 50)
@@ -22,26 +47,25 @@ while True:
     try:
         response = client.chat.completions.create(
             model = 'phi4-mini',
-            messages = [
-                {
-                    "role": "system",
-                    "content": (
-                        "You are an expert coding assistant."
-                    )
-                }
-            ] + conversation,
+            messages = conversation,
             stream = True  # Enable streaming responses
         )
 
         full_response = ""
+
+        print("🤖: ", end = "", flush = True)
+
+        # Stream output response
         for chunk in response:
             content = chunk.choices[0].delta.content or ""
             print(content, end = "", flush = True)  # Print the streamed content in real-time
             full_response += content
 
-        print()  # Print a newline after the full response is received
+        print("\n")  # Print a newline after the full response is received
 
         conversation.append({ "role": "assistant", "content": full_response })  # Add the assistant's response to the conversation history
+        
+        save_conversation(chat_file, conversation)  # Save the conversation history to a file
     except Exception as e:
         print(f"Error: {e}")
         conversation.pop()  # Remove the last unanswered user message from history if there's an error
